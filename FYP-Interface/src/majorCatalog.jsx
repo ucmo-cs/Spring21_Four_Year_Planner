2550513846b0f6e0bc1dc25fe8e466859ccda45d
ForgeThrall <forgemastersemail@gmail.com>
2021-04-27 20:50:55 -0500

    Validate parent sections

--- FYP-Interface/src/majorCatalog.jsx
+++ FYP-Interface/src/majorCatalog.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 
 export default class MajorCatalog extends React.Component {
-	expandSection(section, depth){ return (
-		<div key={section.title}>
-			<div className="catalogSection" style={{marginLeft: depth*16, paddingLeft: 8}}>
-				{section.title}
-			</div>
-			{section.sections?.map(s => this.expandSection(s, depth+1))}
-		</div>
-	)}
+	expandSection(section, depth=0){
+
+		const childrenSections = section.sections?.map(s => this.expandSection(s, depth+1));
+		let areChildrenSatisfied = true;
+		if(childrenSections){
+			for(const child of childrenSections) areChildrenSatisfied = areChildrenSatisfied && child.isSatisfied;
+		} else {
+			// TODO: check validCourses against enrolled courses
+		}
+
+		return ({
+			isSatisfied: true,
+			display: [
+				<div
+					className="catalogSection"
+					key={section.title}
+					style={{marginLeft: depth*16, paddingLeft: 8}}
+				>
+					{section.title}
+				</div>,
+				childrenSections?.map(c => c.display)
+			]
+		})
+	}
 	render() { return (
 		<div className="catalog">
-			{this.expandSection(this.props.catalog, 0)}
+			{this.expandSection(this.props.catalog).display}
 		</div>
 	)}
 }
